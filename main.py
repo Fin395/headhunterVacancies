bd211d6f0d377352619f0cf22e7a6cc8ccfae7b6
Sergei Finogenov <serfinogenov@yandex.ru>
2025-03-25 22:16:17 +0300

    adding supporting method "filter_by_words" to filter the vacancies in accordance with inputted keywords

--- main.py
+++ main.py
@@ -1,15 +1,29 @@
 from head_hunter_api import HeadHunter
-from src.utils import filter_by_salary
-from src.vacancy_processing import Vacancy, sort_vacancies
+from src.utils import filter_by_salary, sort_vacancies, filter_by_words
+from src.vacancy_processing import Vacancy
 
-if __name__ == "__main__":
-    hh_api = HeadHunter()
-    hh_vacancies = hh_api.get_vacancies("python")
+
+hh_api = HeadHunter()
+
+
+def user_interaction():
+    user_search_query = input("Введите ключевое слово для поиска вакансий: ") # Пример: "python"
+    hh_vacancies = hh_api.get_vacancies(user_search_query)
     vacancies_list = Vacancy.from_dict(hh_vacancies)
-    ranged_vacancies = filter_by_salary(vacancies_list, "200000 - 300000")
+    filter_words = input("Введите ключевые слова для фильтрации вакансий: ").lower().split()
+    filtered_vacancies = filter_by_words(vacancies_list, filter_words)
+
+    user_salary_range = input("Укажите диапазон зарплат (в формате 'от'-'до'): ") # Пример: 100000 - 150000
+    ranged_vacancies = filter_by_salary(filtered_vacancies, user_salary_range)
     sorted_vacancies = sort_vacancies(ranged_vacancies)
+
+    # top_n = int(input("Введите количество вакансий для вывода в топ N: "))
+    # filter_words = input("Введите ключевые слова для фильтрации вакансий: ").split()
+    # filtered_vacancies = filter_vacancies(vacancies_list, filter_words)
+    # top_vacancies = get_top_vacancies(sorted_vacancies, top_n)
+    # print_vacancies(top_vacancies)
     for vacancy in sorted_vacancies:
         print(vacancy)
 
-
-
+if __name__ == "__main__":
+    user_interaction()
--- src/utils.py
+++ src/utils.py
@@ -1,12 +1,17 @@
-def filter_by_salary(vacancies_list, salary_range):
+def filter_by_words(vacancies: list, words: list) -> list:
+    vacancies_filtered_by_words = []
+    for word in words:
+        for vacancy in vacancies:
+            if word in vacancy.requirements.lower():
+                vacancies_filtered_by_words.append(vacancy)
+    return vacancies_filtered_by_words
+
+def filter_by_salary(vacancies_list: list, salary_range: str) -> list:
     salary_range_from = int(salary_range.split("-")[0])
     salary_range_to = int(salary_range.split("-")[1])
-
-    vacancies_filtered_by_salary = filter(lambda x: salary_range_to >= x >= salary_range_from, vacancies_list)
+    vacancies_filtered_by_salary = list(filter(lambda x: salary_range_to >= x >= salary_range_from, vacancies_list))
     return vacancies_filtered_by_salary
 
-def sort_vacancies(list_vacancies_objects):
+def sort_vacancies(list_vacancies_objects: list) -> list:
     sorted_vacancies = sorted(list_vacancies_objects, key=lambda x: x.salary, reverse = True)
     return sorted_vacancies
-
-
