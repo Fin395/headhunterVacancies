79fb53d4618c8cef106fe65553ce2b178baed984
Sergei Finogenov <serfinogenov@yandex.ru>
2025-03-25 18:02:55 +0300

    creating magic methods "__le__" and "__ge__" to filter the vacancies

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
-# from head_hunter_api import HeadHunter
-# from src.vacancy_processing import Vacancy
-#
-# if __name__ == "__main__":
-#     hh = HeadHunter()
-#     vacancies = hh.get_vacancies("python")
-#     vacancies_obj = Vacancy.from_dict(vacancies)
-#     for i in vacancies_obj:
-#         print(i.salary)
+from head_hunter_api import HeadHunter
+from src.vacancy_processing import Vacancy
+
+if __name__ == "__main__":
+    hh = HeadHunter()
+    vacancies = hh.get_vacancies("python")
+    vacancies_obj = Vacancy.from_dict(vacancies)
+
+
+
+
+
+
+
+
+    for i in vacancies_obj:
+        print(i)
 
--- src/vacancy_processing.py
+++ src/vacancy_processing.py
@@ -5,12 +5,20 @@ class Vacancy:
         self.name = name
         self.url = url
         self.salary = salary
-        self.validate_salary(salary)
+        self.__validate_salary(salary)
         self.requirements = requirements
-        self.validate_requirement(requirements)
+        self.__validate_requirement(requirements)
 
     def __str__(self):
         return f"Вакансия: {self.name}, ссылка на вакансию: {self.url}, зарплата: {self.salary}, требования: {self.requirements}"
+
+    def __le__(self, other):
+        if isinstance(self.salary, other):
+            return self.salary <= other
+
+    def __ge__(self, other):
+        if isinstance(self.salary, other):
+            return self.salary >= other
 
 
     @classmethod
@@ -21,7 +29,7 @@ class Vacancy:
             vacancy_obj_list.append(cls(name, url, salary, requirements))
         return vacancy_obj_list
 
-    def validate_salary(self, salary):
+    def __validate_salary(self, salary):
         if salary is None:
             self.salary = 0
         elif isinstance(salary, dict):
@@ -33,7 +41,7 @@ class Vacancy:
                 self.salary = salary["to"]
         return self.salary
 
-    def validate_requirement(self, requirements):
+    def __validate_requirement(self, requirements):
         if requirements is None:
             self.requirements = "Не указано"
         return self.requirements
