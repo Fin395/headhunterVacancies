f804807e88a9f1378a62d6b21867de46fb2f0529
Sergei Finogenov <serfinogenov@yandex.ru>
2025-04-02 02:31:41 +0300

    creating the tests for magic methods "__str__", "__lt__", "__gt__" of the "Vacancy" class object

--- tests/test_vacancy.py
+++ tests/test_vacancy.py
@@ -5,10 +5,23 @@ def test_vacancy_init(first_vacancy):
     assert first_vacancy.url == "https://hh.ru/employer/3499705"
     assert first_vacancy.requirement == "Занимать активную жизненную позицию, уметь активно танцевать и громко петь. Обладать навыками коммуникации, чтобы налаживать добрососедские отношения. Обладать системным мышлением..."
 
+
 def test_vacancy_init_none_salary(first_vacancy_none_salary):
     assert first_vacancy_none_salary.id == 93353083
     assert first_vacancy_none_salary.name == "Тестировщик комфорта квартир"
     assert first_vacancy_none_salary.salary == 0
     assert first_vacancy_none_salary.url == "https://hh.ru/employer/3499705"
     assert first_vacancy_none_salary.requirement == "Занимать активную жизненную позицию, уметь активно танцевать и громко петь. Обладать навыками коммуникации, чтобы налаживать добрососедские отношения. Обладать системным мышлением..."
+
+
+def test_vacancy_str(first_vacancy):
+    assert str(first_vacancy) == "id вакансии: 93353083, наименование вакансии: Тестировщик комфорта квартир, зарплата: 450000, ссылка на вакансию: https://hh.ru/employer/3499705, требования: Занимать активную жизненную позицию, уметь активно танцевать и громко петь. Обладать навыками коммуникации, чтобы налаживать добрососедские отношения. Обладать системным мышлением..."
+
+
+def test_vacancy_lt_and_gt(first_vacancy, second_vacancy):
+    result1 = first_vacancy < second_vacancy
+    result2 = first_vacancy > second_vacancy
+    assert result1 is False
+    assert result2 is True
+
 
