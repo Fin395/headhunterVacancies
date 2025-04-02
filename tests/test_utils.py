64d67e76165a8ef2c96e2199a04f60c207b7f280
Sergei Finogenov <serfinogenov@yandex.ru>
2025-04-03 02:32:00 +0300

    creating a test for "sort_vacancies"

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,7 +1,7 @@
 import pytest
 
 from src.exception import InputException
-from src.utils import filter_by_words
+from src.utils import filter_by_words, sort_vacancies
 from src.vacancy import Vacancy
 
 
@@ -12,11 +12,6 @@ def test_filter_by_words(vacancies_obj_list, keywords_for_filter):
      assert filtered_vacancies[1].id == 92223756
 
 
-     # for vacancy in filtered_vacancies:
-     #     assert 1 = Vacancy(93353083, "Тестировщик комфорта квартир", {"from": 350000, "to": 450000}, "https://hh.ru/employer/3499705", "Занимать активную жизненную позицию, уметь активно танцевать и громко петь. Обладать навыками коммуникации, чтобы налаживать добрососедские отношения. Обладать системным мышлением...")
-     # filtered_vacancy2 = Vacancy(92223756, "Удаленный диспетчер чатов (в Яндекс)", {"from": 30000, "to": 44000}, "https://hh.ru/employer/9498120", "Занимать активную жизненную позицию, уметь активно танцевать и громко петь. Обладать навыками коммуникации, чтобы налаживать добрососедские отношения. Обладать системным мышлением...")
-     # assert result == [filtered_vacancy1, filtered_vacancy2]
-
 def test_filter_by_words_no_keywords(vacancies_obj_list, no_keywords):
     with pytest.raises(InputException, match="Введите слова для поиска."):
         filter_by_words(vacancies_obj_list, no_keywords)
@@ -25,3 +20,14 @@ def test_filter_by_words_no_keywords(vacancies_obj_list, no_keywords):
 def test_filter_by_words_bad_keywords(vacancies_obj_list, bad_keywords):
     with pytest.raises(InputException, match="Вакансий с таким описанием не найдено"):
         filter_by_words(vacancies_obj_list, bad_keywords)
+
+
+def test_sort_vacancies(vacancies_obj_list):
+    sorted_vacancies = sort_vacancies(vacancies_obj_list)
+    assert len(sorted_vacancies) == 3
+    assert sorted_vacancies[0].id == 92223756
+    assert sorted_vacancies[1].id == 92223870
+    assert sorted_vacancies[2].id == 93353083
+
+
+
